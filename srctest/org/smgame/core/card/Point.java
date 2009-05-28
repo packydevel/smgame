812147b7533db51fba11d2ffb515cea617db42c5
packydevel@gmail.com <packydevel@gmail.com@dcd7f516-2215-11de-9288-5f72e443383c>
2009-05-28 08:12:11 +0000

    new folder

---
+++ srctest/org/smgame/core/card/Point.java
@@ -0,0 +1,11 @@
+package org.smgame.core.card;
+
+/**Enumerativo TipoCarta
+ *
+ * @author pasquale
+ * @author luca
+ */
+public enum Point {
+    Asso, Due, Tre, Quattro, Cinque,
+    Sei, Sette, Donna, Fante, Re
+} //end enum
---
+++ srctest/org/smgame/core/player/CPUPlayer.java
@@ -0,0 +1,38 @@
+package org.smgame.core.player;
+
+import java.io.Serializable;
+
+/**Classe giocatore CPU
+ *
+ * @author luca
+ * @author pasquale
+ */
+public class CPUPlayer extends Player implements Serializable {
+
+    private final double MAX_CREDIT = 100.0; //massimo credito
+    private final double MIN_SCORE = 4.5; //minimo punteggio
+
+    public CPUPlayer(String name) {
+        super(name);
+    }
+
+    /**Chiede un'altra carta
+     *
+     * @return
+     */
+    public boolean askAnotherCard() {
+        if (getScore() < MIN_SCORE) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**richiede una puntata
+     *
+     * @return
+     */
+    public double requestBet() {
+        return credit / 10 + 1;
+    }
+}
