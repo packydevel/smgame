41b6c1f64056d4f0d99fa83edcf510289f33e33e
judgejud <judgejud@dcd7f516-2215-11de-9288-5f72e443383c>
2009-06-18 11:31:35 +0000

    javadoc core terminato

--- srctest/org/smgame/util/Common.java
+++ srctest/org/smgame/util/Common.java
@@ -1,9 +1,8 @@
 package org.smgame.util;
 
 import java.io.File;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Types;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -16,8 +15,10 @@ public class Common {
 
     final static String separ = File.separator;
     final static String curDir = "c:" + separ + "smgamelog";
-    final static String dirResource = separ + "org" + separ + "smgame" + separ + "resource" + separ;
-    final static String dirResourceCard = dirResource + "cardimage" + separ;
+    //final static String dirResource = separ + "org" + separ + "smgame" + separ + "resource" + separ;
+    final static String dirResource = "/org/smgame/resource/";
+    //final static String dirResourceCard = dirResource + "cardimage" + separ;
+    final static String dirResourceCard = dirResource + "cardimage/";
 
     /**Restituisce il percorso di lavoro corrente comprensivo di primo separatore
      *
@@ -68,31 +69,20 @@ public class Common {
         return data;
     }
 
-    /**imposta i tipi di valore da usare per la preparedStatement
+    /**Converte un percorso stringa in una URL
      *
-     * @param stmt preparedstatement
-     * @param index indice
-     * @param value valore
-     * @param type tipo
-     * @throws java.sql.SQLException
-     * @throws java.lang.Exception
+     * @param path percorso
+     * @return url
      */
-    public static void setParameter(PreparedStatement stmt, int index,
-            Object value, int type) throws SQLException, Exception {
-        if (value == null) {
-            stmt.setNull(index, type);
-        } else {
-            if (type == Types.VARCHAR) {
-                stmt.setString(index, (String) value);
-            } else if (type == Types.INTEGER) {
-                stmt.setInt(index, ((Integer) value).intValue());
-            } else if (type == Types.BIGINT) {
-                stmt.setLong(index, ((Long) value).longValue());
-            } else if (type == Types.DOUBLE) {
-                stmt.setDouble(index, ((Double) value).doubleValue());
-            } else {
-                throw new Exception("Tipo di dato non gestito");
-            }
-        } //end
-    } //end setParameter
+    public static URL convertStringToURL(String path){
+        URL url=null;
+        if (separ.equalsIgnoreCase("\\")){
+            path.replace('\\', '/');
+        }
+        try {
+            url = new URL(path);
+        } catch (MalformedURLException ex) {
+        }
+        return url;
+    }
 }//end class
