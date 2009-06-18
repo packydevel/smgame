eef2cd32cba502cfb7dcf41a9f569e038fb7c3d1
judgejud <judgejud@dcd7f516-2215-11de-9288-5f72e443383c>
2009-06-18 22:55:33 +0000

    javadoc core terminato

--- srctest/org/smgame/client/frontend/StoryBoardJP.java
+++ srctest/org/smgame/client/frontend/StoryBoardJP.java
@@ -2,6 +2,13 @@ package org.smgame.client.frontend;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -9,6 +16,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
+import org.smgame.backend.DBTransactions;
 
 /**Pannello storico partite
  *
@@ -18,31 +26,63 @@ import javax.swing.table.TableColumn;
 public class StoryBoardJP extends JPanel{
 
     private JTable storyboardJT;
+    JButton nextJB, previousJB;
     private int counter;
 
     public StoryBoardJP(Object[][] data) {
         setPreferredSize(new Dimension(400, 250));
         setLayout(new BorderLayout());
 
-        storyboardJT = new JTable();
-        storyboardJT.setModel(tableModel(data));
+        storyboardJT = new JTable();        
         storyboardJT.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         storyboardJT.setFocusable(false);
         storyboardJT.getTableHeader().setReorderingAllowed(false);
-
-        setWitdhColumn(0, 70);
-        setWitdhColumn(1, 140);
-        setWitdhColumn(2, 85);
-        setWitdhColumn(3, 100);
+        setTableModel(tableModel(data));
+        
+        
 
         add(new JScrollPane(storyboardJT), BorderLayout.CENTER);
         setVisible(true);
-        counter = 1;
+        counter = 0;
 
 
-        JButton nextJB = new JButton("Successiva");
-        JButton previousJB = new JButton("Precedente");
+        nextJB = new JButton("Successiva");
+        nextJB.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent evt) {
+                try {
+                    nextGames();
+                } catch (ClassNotFoundException ex) {
+
+                } catch (SQLException ex) {
+
+                } catch (IOException ex) {
+
+                } catch (Exception ex) {
+
+                }
+            }
+        });
+
+        previousJB = new JButton("Precedente");
         previousJB.setEnabled(false);
+        previousJB.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent evt) {
+                try {
+                    previousGames();
+                } catch (ClassNotFoundException ex) {
+
+                } catch (SQLException ex) {
+
+                } catch (IOException ex) {
+
+                } catch (Exception ex) {
+                    
+                }
+            }
+        });
+
         JLabel gameIdJL = new JLabel();
 
         JPanel northJP = new JPanel(new BorderLayout());
@@ -80,5 +120,38 @@ public class StoryBoardJP extends JPanel{
         col.setMinWidth(width);
         col.setPreferredWidth(width);
         col.setMaxWidth(width);
+    }
+
+    private void nextGames() throws ClassNotFoundException, SQLException, IOException, Exception{
+        int size = DBTransactions.sizeStoryGames();
+        if (size > (counter + 1)){
+            DBTransactions dbt = new DBTransactions();
+            setTableModel(tableModel(dbt.getStoryGame(++counter)));
+        }
+        if (counter == 1)
+            previousJB.setEnabled(true);
+        if (size == (counter + 1)){
+            nextJB.setEnabled(false);
+        }
+    }
+
+    private void previousGames() throws ClassNotFoundException, SQLException, IOException, Exception{
+        int size = DBTransactions.sizeStoryGames();
+        if (counter > 0){
+            DBTransactions dbt = new DBTransactions();
+            setTableModel(tableModel(dbt.getStoryGame(--counter)));
+        }
+        if (size > (counter + 1))
+            nextJB.setEnabled(true);
+        if (counter == 0)
+            previousJB.setEnabled(false);
+    }
+
+    private void setTableModel(DefaultTableModel dtm){
+        storyboardJT.setModel(dtm);
+        setWitdhColumn(0, 55);
+        setWitdhColumn(1, 150);
+        setWitdhColumn(2, 80);
+        setWitdhColumn(3, 95);
     }
 }
