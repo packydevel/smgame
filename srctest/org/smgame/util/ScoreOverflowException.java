b05dff639898fd23b479ce37d6bda41d71f18c19
judgejud <judgejud@dcd7f516-2215-11de-9288-5f72e443383c>
2009-06-19 08:20:42 +0000

    javadoc core terminato

--- srctest/org/smgame/util/ScoreOverflowException.java
+++ srctest/org/smgame/util/ScoreOverflowException.java
@@ -2,7 +2,7 @@ package org.smgame.util;
 
 import org.smgame.core.card.Card;
 
-/**Classe gioco
+/**Classe eccezione di punteggio oltre il sette e mezzo
  *
  * @author Traetta  Pasquale 450428
  * @author Mignogna Luca     467644
@@ -11,11 +11,20 @@ public class ScoreOverflowException extends Exception {
 
     Card card;
 
+    /**Costruttore
+     *
+     * @param message messaggio d'errore
+     * @param card carta che ha generato l'overflow
+     */
     public ScoreOverflowException(String message, Card card) {
         super(message);
         this.card=card;
     }
 
+    /**restituisce la carta overflow
+     *
+     * @return carta
+     */
     public Card getCardException(){
         return card;
     }
